6f2a614806973a2e84544a8dfe439c6536d1723a
Drew May <drewm135@gmail.com>
2015-03-03 14:37:13 -0800

    Removed Debug folder

--- Debug/tm4c1294ncpdt_startup_ccs.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-tm4c1294ncpdt_startup_ccs.obj: ../tm4c1294ncpdt_startup_ccs.c
-tm4c1294ncpdt_startup_ccs.obj: /home/keith/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdint.h
-
-../tm4c1294ncpdt_startup_ccs.c: 
-/home/keith/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdint.h: 
